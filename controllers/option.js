d6de0c38380d217fbd1e04903694c13523ea20c3
ankur <ankur.sh2011@gmil.com>
2022-12-24 20:34:36 +0530

    all routes tested

--- controllers/option.js
+++ controllers/option.js
@@ -7,163 +7,6 @@ const Questions= require('../models/question');
 
 
 
-const router= express.Router();
-
-// const getStudents = async (req, res) => {
-//     try {
-//         const student= await Student.find();
-        
-//         res.status(200).json(student);
-//     } catch(error) {
-//         res.status(404).json({message: error.message});
-//     }
-// }
-
-// const getspecStudent = async (req,res) => {
-//     const roll = req.params.roll;
-
-//     try {
-//         const stud = await Student.findOne({roll: roll});
-
-//         res.status(200).json(stud);
-//     } catch(error) {
-//         res.status(404).json({ message: error.message});
-//     }
-// }
-
-// const createoption =  async (req, res) => {
-//     console.log(req.body.title);
-//     const newoption = new Option({
-//         // name:req.body.name,
-//         // roll:req.body.roll,
-//         // registration:req.body.registration,
-//         // subjects:req.body.subjects,
-//         // created_on:req.body.created_on
-//         // title:req.body.title,
-//         // option:req.body.option,
-//         id:req.body.id,
-//         text:req.body.text,
-
-
-//     })
-//     try {
-//      const ankur=   await newoption.save();
-//      console.log("ankur",ankur);
-
-//         res.status(201).json(newoption);
-
-//     } catch(error) {
-//         res.status(400).json({ message : error.message});
-//     }
-
-// }
-
-// const updatestudent = async (req, res) => {
-//     const roll= req.params.roll;
-//     try{
-//         await Student.findOneAndUpdate({
-//             roll: roll,
-//         },
-//         {   
-//             name:req.body.name,
-//             registration:req.body.registration,
-//             subjects:req.body.subjects,
-//             created_on:req.body.created_on
-//         }
-//         )
-//         res.status(202).json({roll: roll});
-
-//     } catch (error) {
-//         res.status(401).json({message: error.message});
-//     }
-    
-// }
-
-// const deleteoption = async (req, res) => {
-//     const id= req.params.id;
-//     console.log("id",id);
-
-//     try {
-//       const ankur =   await Option.findOneAndRemove({id: id});
-//       console.log("ankur",ankur);
-//         res.status(203).json({message:"deleted successfully",id:id});
-
-//     }catch(error) {
-//         res.status(402).json({message: error.message});
-//     }
-// }
-
-// const addVote =  async(req,res)=>{
-//     // const id= req.params.id;
-//      try {
-//         const option = await Option.findById(req.params.id);
-//         // increamenting thee votes
-
-//         option.votes +=1;
-//         await option.save();
-//         // returning the response
-//         return res.status(200).json({
-
-//             message:"vote added",
-//             votes: option.votes,
-//         });
-
-//      } catch(err){
-//         // checking for the error
-//         res.status(465).json({
-//             message:"could not increment the count",
-//             err:"internal server error"
-//         });
-//      }
-
-
-// }
-
-
-// const addOption =  async (req,res) =>{
-//     try {
-//         const question = await Questions.findById(req.params.id);
-//     for (let option of req.body.options) {
-//       // creating options
-//       const currOption = await Option.create({
-//         text: option,
-//       });
-//       // creating dynamic link
-//       currOption.link_to_vote =
-//         "http://" +
-//         req.headers.host +
-//         "/options/" +
-//         currOption.id +
-//         "/add_vote";
-//       currOption.save();
-//       // pushing options id into question
-//       question.options.push(currOption.id);
-//       question.save();
-//     }
-//     // returning the resoponse
-//     return res.status(200).json({
-//       message: "option added succesfully",
-//     });
-//   } catch (err) {
-//     // checking for error
-//     return res.status(465).json({
-//       message: "internal server error",
-//       error: err.message,
-//     });
-//   }
-// };
-
-
-
-// module.exports.getStudents= getStudents;
-// module.exports.createoption= createoption;
-// module.exports.getspecStudent= getspecStudent;
-// module.exports.updatestudent= updatestudent;
-// module.exports.deleteoption= deleteoption;
-// module.exports.addVote=addVote;
-// module.exports.addOption=addOption;
-
-
 //To add options to a specific question
  const createNewOption = async function(req, res){
 
@@ -188,11 +31,8 @@ const router= express.Router();
          let ankur =  await option.save();
           console.log("ankur",ankur);
           // question.push(option.link_vote)
-
-          
-        // let joi=  question.options.push(option.link);
-         let amber= await  question.save();
-         console.log("amber",amber)
+         let qu= await  question.save();
+         console.log("amber",qu)
 
           return res.status(200).json({option, data:{
               "message" : "New Option created Successfully"
@@ -210,35 +50,40 @@ const router= express.Router();
 };
   
   //To delete an option
-  const deleteOption = async function (req, res) {
-    try {
-      // finding the particular option
-      const option = await Option.findById(req.params.id);
-      // checking whether it contains any vote or not
-      if (option.votes > 0) {
-        return res.status(401).json({
-          message: "You cannot delete that vote",
+  const deleteOption = async function(req, res){
+
+    try{
+
+        let id = req.params.id;
+
+        // checking if option exists
+        let option = await Option.findById(id);
+
+        //checking if number of votes are > 0, if true, an option should not be deleted
+        if(option.votes > 0){
+            return res.status(404).json({
+                data : { message : "Option can not be deleted, count of votes > 0 " }
+            });
+        }
+
+        // deleting option from question.options array first
+        await Questions.findByIdAndUpdate(option.question, {$pull : {options : id}});
+
+        // now deleting the option from the db
+        await Option.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            data : { message : "Option deleted sucessfully !" }
         });
-      }
-      // finding the question and updating it
-      await Questions.updateOne(
-        { options: { $in: req.params.id } },
-        { $pull: { options: { $eq: req.params.id } } }
-      );
-      // deleting the particular option
-      await option.remove();
-      // returning the response
-      return res.status(200).json({
-        message: "option deleted succesfully",
-      });
-    } catch (err) {
-      // checking for error
-      return res.status(465).json({
-        message: "internal server error",
-        error: err.message,
-      });
+
+    }catch(err){
+        console.log(" Error in deleting option ",err);
+        return res.status(500).json({
+            data : { message : "Internal Server Error in deleting vote" }
+        });  
     }
-  };
+
+};
   
   //To increment the count of votes
  const incrementVotes = async function (req, res) {
--- controllers/question.js
+++ controllers/question.js
@@ -5,32 +5,6 @@ const Questions= require('../models/question');
 const Option= require('../models/option');
 
 
-// const router= express.Router();
-
-// const getStudents = async (req, res) => {
-//     try {
-//         var id = req.params.id
-//         const questions= await Question.findById(id);
-//         console.log("questions",questions);
-        
-//         res.status(200).json(questions);
-//     } catch(error) {
-//         res.status(404).json({message: error.message});
-//     }
-// }
-
-// const getspecStudent = async (req,res) => {
-//     const roll = req.params.roll;
-
-//     try {
-//         const stud = await Student.findOne({roll: roll});
-
-//         res.status(200).json(stud);
-//     } catch(error) {
-//         res.status(404).json({ message: error.message});
-//     }
-// }
-
 const createQuestion =  async (req, res) => {
     console.log(req.body.title);
     const questions = new Questions({
@@ -56,99 +30,56 @@ const createQuestion =  async (req, res) => {
 
 }
 
-// const updatestudent = async (req, res) => {
-//     const id= req.params.id;
-//     try{
-//         await Student.findOneAndUpdate({
-//             id: id,
-//         },
-//         {   
-//             name:req.body.name,
-//             registration:req.body.registration,
-//             subjects:req.body.subjects,
-//             created_on:req.body.created_on
-//         }
-//         )
-//         res.status(202).json({roll: roll});
-
-//     } catch (error) {
-//         res.status(401).json({message: error.message});
-//     }
-    
-// }
-
-// const deletequestion = async (req, res) => {
-//     const id= req.params.id;
-
-//     try {
-//       const ankur=  await Question.findOneAndRemove({id: id});
-//       console.log("ankur",ankur);
-//         res.status(203).json({message:'deleted successfulyy',id:id});
-
-//     }catch(error) {
-//         res.status(402).json({message: error.message});
-//     }
-// }
-
-// module.exports.getStudents= getStudents;
-// module.exports.createstudent= createstudent;
-// module.exports.getspecStudent= getspecStudent;
-// module.exports.updatestudent= updatestudent;
-// module.exports.deletequestion= deletequestion;
 
 
 
-//To create a question
-// const createQuestion = async function (req, res) {
-//     try {
-//       // creating the questions
-//       for (let title of req.body.title) {
-//         await Questions.create({ title });
-//       }
-//       // returning the resoponse
-//       return res.status(200).json({
-//         message: "question created succesfully",
-//       });
-//     } catch (err) {
-//       // checking for the error
-//       return res.status(465).json({
-//         message: "error in creating a questions",
-//         error: err.message,
-//       });
-//     }
-//   };
   
   //To delete a question
-  const deleteQuestion = async function (req, res) {
-    try {
-      // finding the particular Question
-      const question = await Questions.findById(req.params.id);
-      // deleting all the options related to that question
-      for (let id of question.options) {
-        let option = await Option.findById(id);
-        // checking whether option contains any votes or not
-        if (option.votes > 0) {
-          return res.status(401).json({
-            message: "you cannot delete that option",
-          });
+const deleteQuestion = async function(req, res){
+
+    try{
+
+        let id = req.params.id;
+        let question = await Question.findById(id).populate({
+            path : 'option',
+            select : 'votes',
+        });
+
+        if(question){
+            // checking if any option has some votes already
+            let options = question.options;
+
+            for(let i = 0; i < options.length; i++){
+                if(options[i].votes > 0){
+                    return res.status(404).json({
+                        data : {
+                            message : "Question can not be deleted, it's options are voted already !",
+                        }
+                    });
+                }
+            }
+
+           
+            await Option.deleteMany({ question:id });
+            await Question.findByIdAndDelete(id);
+
+            return res.status(200).json({
+                message : "Question deleted successfully",
+            });
+
+        }else{
+            return res.status(404).json({ message : "Question not found" });
         }
-        // delete that particular option
-        await option.remove();
-      }
-      // deleting the question
-      await question.remove();
-      // sending response
-      return res.status(200).json({
-        message: "question deleted succesfully",
-      });
-    } catch (err) {
-      // checking for error
-      return res.status(465).json({
-        message: "internal server error",
-        error: err.message,
-      });
+
+    }catch(err){
+        console.log("******* Error in deleting question ********* ",err);
+        return res.status(500).json({
+            message : "Internal server error in deleting question",
+        });
     }
-  };
+
+}
+    
   
   //To view a question and it’s options
 const viewQuestion = async function(req, res){
