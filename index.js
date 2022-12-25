ec3eeadac9bb8541ffd3f4af69996b205c7b8a50
ankur <ankur.sh2011@gmil.com>
2022-12-25 08:17:22 +0530

    resolved problem in mongoose link

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const express= require('express');
 const mongoose= require('mongoose');
 const questionrouter= require("./routes/questions");
 const optionrouter = require("./routes/options");
-const config =  require("./config/mongoose")
+// const config =  require("./config/mongoose")
 
 
 const app=express();
 
 const port=9000;
-// const url= "mongodb+srv://Ankuristic12:Adya1998@cluster0.0lufvph.mongodb.net/?retryWrites=true&w=majority";
-// console.log("url",url);
+const url= "mongodb+srv://Ankuristic12:Adya1998@cluster0.0lufvph.mongodb.net/?retryWrites=true&w=majority";
+console.log("url",url);
 
-// mongoose.connect(url,{useNewUrlParser: true});
-// const con= mongoose.connection;
-// app.use(express.json());
-// try{
-//     con.on('open',() => {
-//         console.log('connected');
-//     })
-// }catch(error)
-// {
-//     console.log("Error: "+error);
-// }
+mongoose.connect(url,{useNewUrlParser: true});
+const con= mongoose.connection;
+app.use(express.json());
+try{
+    con.on('open',() => {
+        console.log('connected');
+    })
+}catch(error)
+{
+    console.log("Error: "+error);
+}
 
 app.use('/questions',questionrouter);
 app.use('/options',optionrouter);
